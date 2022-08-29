1dbb0102649c910ae0912977d2102e4b9ca3d786
derplime <edolorfino24@gmail.com>
2022-08-29 16:18:39 -0400

    added sample 1 implentation part 1

--- sample3/src/cljs/sample3/core.cljs
+++ sample3/src/cljs/sample3/core.cljs
@@ -40,6 +40,71 @@
   [:section.section>div.container>div.content
    [:img {:src "/img/warning_clojure.png"}]])
 
+(comment
+
+  (def app-data (r/atom {:x 0 :y 0 :total 0}))
+
+  (defn swap [val]
+        (swap! app-data assoc
+               :total val)
+        (js/console.log "The value from plus API is" (str (:total @app-data)))); Value comes out in console
+
+  (defn math [params operation]
+        (POST (str "/api/math/" operation)
+              {:headers {"accept" "application/transit-json"}
+               :params  @params
+               :handler #(swap (:total %))}))
+
+  (defn getAdd []
+        (GET "/api/math/plus?x=1&y=2"
+             {:headers {"accept" "application/json"}
+              :handler #(swap (:total %))}))
+
+  ; TODO - update to clojure parseInt
+  (defn int-value [v]
+        (-> v .-target .-value int))
+
+  ; Function to update the color of the p tag class
+  (defn change-color []
+        (cond
+          (<= 0 (:total @app-data) 19) {:style {:color "lightgreen" :font-weight :bold}}
+          (<= 20 (:total @app-data) 49) {:style {:color "lightblue" :font-weight :bold}}
+          :default {:style {:color "lightsalmon" :font-weight :bold}}))
+
+  (:total @app-data)
+  (POST "/api/math/plus"
+        {:headers {"accept" "application/transit-json"}
+         :params  {:x 1 :y 2}
+         :handler #(swap (:total %))})
+
+  (<= 0 34 48)
+
+  (defn home-page []
+        (let [params (r/atom {})]
+             [:section.section>div.container>div.content
+              [:h1 "Hello World!"]
+              [:h3 "Button to add 1 + 2. Result in text at the bottom."]
+              [:button.button.is-primary {:on-click #(getAdd)} "1 + 2"]
+              [:h3 "Fill out fields and select the desired operation. Result in text at the bottom."]
+              [:form
+               [:div.form-group
+                [:label "Value 1: "]
+                [:input {:type :text :placeholder "0" :on-change #(swap! params assoc :x (int-value %))}]]
+               [:div.form-group
+                [:label "Value 2: "]
+                [:input {:type :text :placeholder "0" :on-change #(swap! params assoc :y (int-value %))}]]]
+              [:br]
+              [:button.button.is-primary {:on-click #(math params "plus")} "+"]
+              [:button.button.is-info {:on-click #(math params "minus")} "-"]
+              [:button.button.is-warning {:on-click #(math params "multiply")} "*"]
+              [:button.button.is-danger {:on-click #(math params "divide")} "/"]
+              [:br]
+              [:br]
+              [:p "Your calculated value is: "
+               [:span (change-color) (:total @app-data)]] ; update the class here
+              ]))
+  )
+
 (defn home-page []
   [:section.section>div.container>div.content
    (when-let [docs @(rf/subscribe [:docs])]
